4f6402ed875450ae2ca35dfc20262dc82d2f66e8
Michael Carroll <carrollm2@gmail.com>
2020-01-12 01:25:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,8 +15,7 @@ class Student
   # end
 
 
-  # def self.new_from_db(row)
-  def self.new_from_db(id=nil, name, grade)
+  def self.new_from_db(row)
     new_student = self.new
     new_student.id = row[0]
     new_student.name =  row[1]
