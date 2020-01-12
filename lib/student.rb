d95e5fe728c7468247ad41c39ad59ed77f762829
Michael Carroll <carrollm2@gmail.com>
2020-01-12 01:38:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,13 +14,12 @@ class Student
     @id = id
     @name = name
     @grade = grade
+    binding.pry
   end
 
 
   def self.new_from_db(row)
     new_student = self.new
-
-    binding.pry
     new_student.id = row[0]
     new_student.name =  row[1]
     new_student.grade = row[2]
